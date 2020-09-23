28363bbe65a985fc5421bdd010b399294f388d4b
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2020-09-23 15:19:16 +0300

    port setting not working, fix

--- back/index.ts
+++ back/index.ts
@@ -14,7 +14,7 @@ app.get('/ping', (_req, res) => {
 app.use('/api/words', wordRouter)
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT ? process.env.PORT : 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+})
