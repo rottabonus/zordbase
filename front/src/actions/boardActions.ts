3357d2d9da4f4f2e5734b211d991e2c02e4cb5a3
rottabonus <felix.hallenberg@myy.haaga-helia.fi>
2020-09-29 16:01:00 +0300

    better letter probability

--- front/src/actions/boardActions.ts
+++ front/src/actions/boardActions.ts
@@ -34,7 +34,7 @@ const changeTurn = (turn: string) => {
 }
 
 const createGameBoard = (rows: number, columns: number) => {
-    const letters = 'aaaaaaaaabcdeeeeeeefgghhhhiiiiiiiijjjjkkkkllllllmmmmmnnnnnnnooooopppprrrrsssssstttttttuuuuvvvvyyööääääää'
+    const letters = 'aaaaaaaaaaaaiiiiiiiiiiittttttttttnnnnnnnnneeeeeeeesssssssslllllloooookkkkkuuuuuääääämmmmvvrrjjhhyyppdö'
     const letterArr = letters.split('')
     const board: string[][] = []
     let rowArray: string[] = []
--- front/src/services/words.ts
+++ front/src/services/words.ts
@@ -1,12 +1,15 @@
+const baseUrl: string = '/api/words/'
+const devUrl: string = 'http://localhost:3000/api/words/'
+
 const fetchAll = async () => {
-    const response = await fetch('/api/words/');
+    const response = await fetch(baseUrl);
     const data = await response.json()
     return data.words
 }
 
 const fetchMatch = async (word: string) => {
   if (word !== '') {
-    const response = await fetch('/api/words/');
+    const response = await fetch(baseUrl);
     const data = await response.json()
     const result = data.words.filter((a: string) => a.toUpperCase() === word)
     return result.length > 0
